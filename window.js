b8514c2ac9569912c74a93c8d259bd1c638e41f2
Your   git config --global user.name Your <atiqnishebd24400@gmail.com>
2021-07-23 23:38:21 +0600

    window global scope and local scope

---
+++ window.js
@@ -0,0 +1,18 @@
+
+var num3 = 30;
+function add(num1, num2) {
+var result = num1+num2;
+    console.log(result);
+    console.log(num3);
+    return result;
+    function dubole(x){
+        var result1 = x*2;
+        return result1;
+    }
+    var total = dub(10)
+    console.log(total);
+}
+console.log(result);
+console.log(num3);
+var sum = add(30,40);
+console.log(sum);
